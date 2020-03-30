243d740e93636331d00f7f4c675ecce605c20023
vagrant <takakoba0426@gmail.com>
2020-03-30 10:51:54 +0000

    [Start]5-6

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ resources :post_images, only: [:new, :create, :index, :show, :destroy] do
     resource :favorites, only: [:create, :destroy]
     resource :post_comments, only: [:create, :destroy]
 end
-resources :users, only: [:show]
+resources :users, only: [:show, :edit, :update]
 end
