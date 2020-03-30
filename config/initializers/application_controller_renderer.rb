194cdf64c2f396ec5b47012ac3768e9e8c902c88
vagrant <takakoba0426@gmail.com>
2020-03-30 07:50:16 +0000

    [Finish]5-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'd47d123f7c96e20f84d57d09a0ff6423f1b5d5288c66186795c67c00673372ec41c28b5f8e2121af570c034f125cc48adccf14d6a2c7e18cc3e5d047034aa4b0'
